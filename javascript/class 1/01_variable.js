af67ba907a6f31bc47082a9f1548b662d1ebe6d7
Akash3047 <101284418+Akash3047@users.noreply.github.com>
2024-05-16 17:05:25 +0530

    all files added

---
+++ javascript/class 1/01_variable.js
@@ -0,0 +1,50 @@
+//declare a variable
+let num = 100;
+console.log(num)  //to print the output
+
+
+let Num = 50;
+console.log(Num);
+
+//let second num = 1000;
+
+let second_num = 1000;   //second_num(snakeCase)
+console.log(second_num);
+
+let secondNum = 1000;   //second_num(camelCase)
+console.log(secondNum);
+
+let decimal_num = 18.35;
+console.log(decimal_num);
+
+console.log(typeof decimal_num);
+
+//task:
+//task1 : declare a variable and assign a value to it.
+//task2 : declare different variable of numbers nd assign different values to perform mathematical operations. 
+//eg- +,-,*,/ and modulus
+
+let num1 = 10;
+let num2 = 20;
+let addition = num1 + num2;
+console.log(addition);
+
+let num3 = 10;
+let num4 = 20;
+let substraction = num3 - num4;
+console.log(substraction);
+
+let num5 = 10;
+let num6 = 20;
+let multiplication = num5 * num6;
+console.log(multiplication);
+
+let num7 = 10;
+let num8 = 40;
+let divide = num7 / num8;
+console.log(divide);
+
+let num9 = 10;
+let num10 = 40;
+let modulus = num9 % num10;
+console.log(modulus);
