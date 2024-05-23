a6306cafeb132f032c5de30eb8e68eff75d259d8
Akash3047 <101284418+Akash3047@users.noreply.github.com>
2024-05-23 15:49:22 +0530

    All files added

---
+++ javascript/class 3/02_iteration.js
@@ -0,0 +1,6 @@
+//iteration:
+
+//what are loops:
+//structure that are allow repeating a block of code multiple times ,used to perform repeative task efficently.
+
+//typ
---
+++ javascript/class 3/03_structure.js
@@ -0,0 +1,6 @@
+//De-stryctured:
+
+//destructuring is a process to extract the values of anarray and object and re-assign them into a new variables.
+//it is a shoertned way to extract values of an array and objects.
+//it is a way to assign the values of an array and object into a new variables.
+
