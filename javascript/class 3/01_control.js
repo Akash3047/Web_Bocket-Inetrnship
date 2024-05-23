49b55d46b0d0453e2b273584b160c1a48952a12d
Akash3047 <101284418+Akash3047@users.noreply.github.com>
2024-05-23 15:24:30 +0530

    All files added

---
+++ javascript/class 3/01_control.js
@@ -0,0 +1,80 @@
+//control flow statement:
+
+//conditional statement:
+//statements that are perform different actions based on differnt condition used to make decision in code.
+
+//Types:
+//1.if statement"
+//2.if-else statement:
+//3.else staetement:
+//4.swith statement:
+//5.terniary statement:
+
+//if statement:
+//synrax:
+// if (condition) {
+    
+// }
+
+// let age = 24;     //declare variable
+// if (age >= 18) {
+//     console.log("You are an adlut");
+// }
+// //You are an adlut
+
+//else statement:
+
+// let age = 17;
+// if (age >= 18) {
+//     console.log("You are an adlut");
+// } else{
+//     console.log("You are not an adult");
+// }
+
+//if-else statement:
+
+// let age = 19;
+// if (age > 18) {
+//     console.log("You are an adlut");
+// } else if(age ==18) {
+//     console.log("You are 18");
+// } else{
+//     console.log("You are not adult");
+// ;}
+
+//switch statement:
+//syntax:
+let day = 5;      //declare a variable
+switch (day) {
+    case 1:
+        console.log("Today is Monaday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thrusday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Satyrday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+        break;
+    //defult:if none of the above case match.
+    default:
+        console.log("Invalid Day");
+        break;
+}
+
+//terniary opearator:
+//syntax:
+
+let age = 15;
+age >18 ? console.log("You are an adult"): console.log("You are  not an adult");
