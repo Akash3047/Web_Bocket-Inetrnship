461ca92109623ef25b08efc6d2815a28ed67b293
Akash3047 <101284418+Akash3047@users.noreply.github.com>
2024-05-24 11:38:26 +0530

    All files added

--- javascript/class 3/03_structure.js
+++ javascript/class 3/03_structure.js
@@ -1,6 +1,39 @@
-//De-stryctured:
+// De-structure :
 
-//destructuring is a process to extract the values of anarray and object and re-assign them into a new variables.
-//it is a shoertned way to extract values of an array and objects.
-//it is a way to assign the values of an array and object into a new variables.
+// destructuring is a process to extract the values of array and object and to re-assign them into a new variables.
+// it is a shorthand way to extract the values of array and object.
 
+// let myArray = ["Pramod", "Rudra", "Akash", "Muna"];
+// let [name1, name2, name3, name4] = myArray; // destructure
+// console.log(myArray);
+// console.log(name1, name2, name3, name4); // output : Pramod Rudra Akash Muna
+// console.log(name1);
+// console.log((name1 = "Smruti"));
+// console.log((myArray[0] = name1));
+// console.log(myArray);
+
+// skipping elements :
+// let [, , name3, name4] = myArray;
+// console.log(name3, name4);
+
+// rest operator :
+// let [name1, name2, ...rest] = myArray;
+// console.log(name1); // Pramod
+// console.log(name2); // Rudra
+// console.log(rest); // ["Akash","Muna"]
+
+// destructure of object
+let student = {
+    name: "John",
+    age: 30,
+    city: "Bhubaneswar",
+    marks: [80, 90, 70],
+    isMarried: false,
+    address: { po: "RRL", pin: 751013 },
+  };
+  
+  // let { name, age, city } = student; // destructure
+  // console.log(name, age, city);
+  
+  let { name, age, ...rest } = student;
+  console.log(name, age, rest);  // rest operator 
