ab9a4bbe97e7de1b438ace913bc8778c645cd01f
Akash3047 <101284418+Akash3047@users.noreply.github.com>
2024-06-07 13:05:22 +0530

    All files added

---
+++ Backend/class 2/data.js
@@ -0,0 +1,14 @@
+const  sum = (num1, num2) =>{
+    return num1 + num2;
+};
+const  substract = (num1, num2) =>{
+    return num1 - num2;
+};
+const  multiply = (num1, num2) =>{
+    return num1 * num2;
+};
+const  division = (num1, num2) =>{
+    return num1 / num2;
+};
+//Export modules:
+module.exports = {sum, substract, multiply, division};
