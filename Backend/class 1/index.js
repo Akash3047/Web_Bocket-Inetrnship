58855831c588130d4d048c3731d2667abf2d3df7
Akash3047 <101284418+Akash3047@users.noreply.github.com>
2024-06-07 11:52:08 +0530

    All files added

--- Backend/class 1/index.js
+++ Backend/class 1/index.js
@@ -6,12 +6,44 @@ const os = require("os");
 //console.log(fs);
 
 
-//crete a server:
-const server = http.createServer((req,res) => {
-    if (req.url === "/") {
-        res.end("Hey welcome to my server");
+// //crete a server:
+// const server = http.createServer((req,res) => {
+//     if (req.url === "/") {
+//         res.end("Hey welcome to my server");
+//     }
+//     else if(req.url == "/Home"){
+//         res.end("Hey welcome to my home page");
+//     }
+//     else if(req.url == "/about"){
+//         res.end("Hey welcome to my about page");
+//     }
+//     else if (req.url == "/contact") {
+//         res.end("hey welcome to my contact page");
+//     }
+//     else{
+//         res.end("404 page not found");
+//     }
+// });
+
+const server = http.createServer((req,res)=>{
+    switch (req.url) {
+        case "/":
+            res.end("Hey welcome to my server");
+            break;
+        case "/home":
+            res.end("Hey welcome to my home");
+            break;
+        case "/about":
+            res.end("Hey welcome to my about");
+            break;
+        case "/contact":
+            res.end("Hey welcome to my contact");
+            break;
+        default:
+            res.end("404 page not found");
+            break;
     }
-});
+}); 
 
     
 //Run start the server
